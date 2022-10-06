b40f18c1daa492ec4d25f327642b505ed8aa0c67
Calvin <ncalvin67@gmail.com>
2022-10-06 23:51:02 +0300

    Formatting

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,72 +1,91 @@
 import React from "react";
-import {useCart} from "react-use-cart"
+import { useCart } from "react-use-cart";
 
 const Card = ({ merch, setCards, cards }) => {
-  const {addItem} = useCart();
+  const { addItem } = useCart();
 
-  function handleDelete(){
-    fetch(`https://react-anime-merch-api.herokuapp.com/data/${merch.id}`,{
+  function handleDelete() {
+    fetch(`https://react-anime-merch-api.herokuapp.com/data/${merch.id}`, {
       method: "DELETE",
     })
-    .then(res => res.json())
-    .then(()=>{
-      const delCard = cards.filter((merchandise)=>(
-        merchandise.id !== merch.id
-      ));
-      setCards(delCard)
-    })
+      .then((res) => res.json())
+      .then(() => {
+        const delCard = cards.filter(
+          (merchandise) => merchandise.id !== merch.id
+        );
+        setCards(delCard);
+      });
   }
 
   return (
     <>
-    <div className="col">
-    <div className="card h-100">
-      <img src={merch.image} className="card-img-top" alt="..." height={330}/>
-      <div className="card-body">
-        <h4 className="card-title">{merch.name}</h4>
-        <small className="text-muted">{merch.type}</small>
-        <p className="card-text">{merch.description}</p>
-        <p className="card-text text-success">=Ksh {merch.price}</p>
-        <div className="card-button row d-flex justify-content-between">
-        <button onClick={()=>addItem(merch)} className="btn btn-primary ms-2 mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal" style={{width: "120px"}}>Add to Cart</button>
-        <button onClick={handleDelete} className="btn btn-danger ms-2 mb-1" style={{width: "120px"}}>Delete</button>
-      </div>
-      </div>
-      </div>
-    </div>
-    <div
-    className="modal fade"
-    id="exampleModal"
-    tabIndex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
-    <div className="modal-dialog">
-      <div className="modal-content">
-        <div className="modal-header">
-          <h1 className="modal-title fs-5" id="exampleModalLabel">
-            Item added
-          </h1>
-          <button
-            type="button"
-            className="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
-        <div className="modal-body">This item has been added to cart</div>
-        <div className="modal-footer">
-          <button
-            type="button"
-            className="btn btn-primary"
-            data-bs-dismiss="modal"
-          >
-            Close
-          </button>
+      <div className="col">
+        <div className="card h-100">
+          <img
+            src={merch.image}
+            className="card-img-top"
+            alt="..."
+            height={330}
+          />
+          <div className="card-body">
+            <h4 className="card-title">{merch.name}</h4>
+            <small className="text-muted">{merch.type}</small>
+            <p className="card-text">{merch.description}</p>
+            <p className="card-text text-success">=Ksh {merch.price}</p>
+            <div className="card-button row d-flex justify-content-between">
+              <button
+                onClick={() => addItem(merch)}
+                className="btn btn-primary ms-2 mb-1"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+                style={{ width: "120px" }}
+              >
+                Add to Cart
+              </button>
+              <button
+                onClick={handleDelete}
+                className="btn btn-danger ms-2 mb-1"
+                style={{ width: "120px" }}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Item added
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">This item has been added to cart</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-primary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,20 +2,24 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ cards, setCards, handleDelete }) => {
-
   const displayCards = cards.map((merch) => (
-    <Card key={merch.id} merch={merch} cards={cards} setCards={setCards} handleDelete={handleDelete} />
+    <Card
+      key={merch.id}
+      merch={merch}
+      cards={cards}
+      setCards={setCards}
+      handleDelete={handleDelete}
+    />
   ));
 
   return (
     <>
-    <div className="container-fluid">
-      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-4">
-        {displayCards}
-      </div>  
-    </div>
-    
-  </>
+      <div className="container-fluid">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-4">
+          {displayCards}
+        </div>
+      </div>
+    </>
   );
 };
 
