f7fc56b7189efa3d7fb791bdd6041cf608ac1ab6
Calvin <ncalvin67@gmail.com>
2022-10-07 09:26:58 +0300

    Added some dummy data to the about page

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,12 @@ const About = () => {
             src='https://getwallpapers.com/wallpaper/full/e/4/0/420625.jpg'
             alt='backdrop-image'
             title='About Page'
-            paragraph=''
-            small=''
+            paragraph1='Welcome to my Anime Merchandise Store, Home to a wide variety of anime collectibles for weebs all over'
+            paragraph2='This is also the only page you can access without signing in because it only has this little amount of dummy data.
+            For more functionalities within the web page I would suggest taking the initiative of signing in and buying some merch.
+            Till then have a great day and be on your way
+            '
+            small='Made by yours truly'
         />
     </div>
   )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,54 +1,65 @@
-import React, { useState, useEffect } from "react";
-import CardList from "./CardList";
-import Pagination from "./Pagination";
+import React, { useState, useEffect } from 'react'
+import CardList from './CardList'
+import Pagination from './Pagination'
 import { Redirect } from "react-router-dom";
-import IntroCard from "./IntroCard";
-import AddItems from "./AddItems";
+import IntroCard from './IntroCard';
+import AddItems from './AddItems';
 
-const Home = ({ isLoggedIn }) => {
-  const [cards, setCards] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [merchPerPage] = useState(4);
+const Home = ({isLoggedIn}) => {
+    const [cards, setCards] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
+    const [merchPerPage] = useState(4)
 
-  useEffect(() => {
-    fetch(`https://react-anime-merch-api.herokuapp.com/data`)
+    useEffect(() => {
+      fetch(`https://react-anime-merch-api.herokuapp.com/data`)
       .then((res) => res.json())
       .then((aniInfo) => {
         setCards(aniInfo);
       });
-  }, [cards]);
+    }, [setCards]);
 
-  const indexOfLastCard = currentPage * merchPerPage;
-  const indexOfFirstCard = indexOfLastCard - merchPerPage;
-  const currentCards = cards.slice(indexOfFirstCard, indexOfLastCard);
+    const indexOfLastCard = currentPage * merchPerPage;
+    const indexOfFirstCard = indexOfLastCard - merchPerPage;
+    const currentCards = cards.slice(indexOfFirstCard, indexOfLastCard);
+    
+    console.log(indexOfLastCard);
+    console.log(indexOfFirstCard);
+    console.log(currentCards);
+    
+    
+    if (!isLoggedIn) return <Redirect to="/login" />
 
-  if (!isLoggedIn) return <Redirect to="/login" />;
-
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+    const paginate = (pageNumber) => setCurrentPage(pageNumber)
 
   return (
     <div>
-      <IntroCard
+    <IntroCard 
         src="https://i.pinimg.com/originals/33/02/ab/3302abb9dbcb083e8acb39a84b274022.png"
         alt="background-image"
         title="Welcome To Your One Stop Anime Store"
-        paragraph1="Adipisicing reprehenderit labore proident aute nisi consectetur enim. Esse enim ipsum duis occaecat id. 
+        paragraph1='Adipisicing reprehenderit labore proident aute nisi consectetur enim. Esse enim ipsum duis occaecat id. 
         Laborum aliquip duis tempor quis excepteur quis est irure consectetur voluptate enim occaecat magna mollit.
-        Esse enim ipsum duis occaecat id. Laborum aliquip duis tempor quis excepteur quis est irure consectetur voluptate enim occaecat magna mollit."
+        Esse enim ipsum duis occaecat id. Laborum aliquip duis tempor quis excepteur quis est irure consectetur voluptate enim occaecat magna mollit.'
         paragraph2="In case you want to know more about this site I suggest visting the About us page. But if you are okay and are just
         eager to see the site functionality then by all means browse away and dont't forget to like and share"
         small="Made By Yours Truly"
-      />
-      <h2 className="mb-3">Anime Merchandise: The Hot Topic Anime Store</h2>
-      <CardList cards={currentCards} setCards={setCards} />
-      <Pagination
+    />
+    <h2 className='mb-3'>Anime Merchandise: The Hot Topic Anime Store</h2>
+    <CardList 
+        cards={currentCards}
+        setCards={setCards}
+    />
+    <Pagination 
         merchPerPage={merchPerPage}
         totalMerch={cards.length}
         paginate={paginate}
-      />
-      <AddItems cards={cards} setCards={setCards} />
+        />
+    <AddItems
+      cards={cards}
+      setCards={setCards}
+    />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
