f090cfa530c4d0fe15d7ffe8640822945981ffd5
Calvin <ncalvin67@gmail.com>
2022-10-08 20:35:45 +0300

    About update

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,10 @@ const About = () => {
             src='https://getwallpapers.com/wallpaper/full/e/4/0/420625.jpg'
             alt='backdrop-image'
             title='About Page'
-            paragraph1='Welcome to my Anime Merchandise Store, Home to a wide variety of anime collectibles for weebs all over'
+            paragraph1='Welcome to my Anime Merchandise Store, Home to a wide variety of anime collectibles for weebs all over.
+            This is a simplistic website that was created by react. It uses bootstrap 5 for styling. Nulla tempor consequat nostrud duis proident. 
+            Laborum amet sint laborum esse sint. Dolor non cillum laborum adipisicing cupidatat deserunt ea quis.
+            Ut laborum occaecat commodo veniam dolore. Ut veniam anim culpa tempor laboris sint est excepteur nulla ex.'
             paragraph2='This is also the only page you can access without signing in because it only has this little amount of dummy data.
             For more functionalities within the web page I would suggest taking the initiative of signing in and buying some merch.
             Till then have a great day and be on your way
